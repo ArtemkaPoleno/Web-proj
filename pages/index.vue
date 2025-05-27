f4f84f716a7863a35f1e99c49a1c0cc12e3ea18f
ArtemkaPoleno <hdh16788@gmail.com>
2025-05-27 16:54:11 +0400

    Create index.vue

---
+++ pages/index.vue
@@ -0,0 +1,119 @@
+<script setup>
+    const truncateText = (text, maxLenght) =>{
+      if(!text) return '';
+      return text.lenght > maxLenght
+      ? text.substring(0, maxLenght) + "..."
+      : text;
+    };
+    const { data: posts } = await useAsyncData('posts', () => 
+    $fetch('http://localhost:3001/posts?_limit=3')
+    );
+</script>
+  <template>
+
+  <div>
+    <AppHeader />
+    <NuxtPage />
+  </div>
+  
+  <div class="post-grid">
+
+    <div class="post" v-for="post in posts" :key="post.id">
+      <h2>{{ truncateText(post.title, 10) }}</h2>
+      <p>{{ truncateText(post.content,20) }}</p>
+    </div>
+  </div>
+
+  <div class="text-box"><NuxtLink to="/posts" class="btn btn-white btn-animate">Посмотреть все посты</NuxtLink></div>
+</template>
+<style lang="scss">
+body{
+  margin: 0;
+  padding-left: 192px;
+  padding-right: 192px;
+
+}
+.content_main{
+  display: flex;
+  align-items: center;
+}
+.post-grid{
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr ;
+  gap: 20px;
+  margin-bottom: 20px;
+}
+.text-box{
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.btn{
+  border: 1px solid;
+}
+.btn:link,
+.btn:visited {
+    text-transform: uppercase;
+    text-decoration: none;
+    padding: 15px 40px;
+    display: inline-block;
+    border-radius: 100px;
+    transition: all .2s;
+    position: absolute;
+}
+
+.btn:hover {
+    transform: translateY(-3px);
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+}
+
+.btn:active {
+    transform: translateY(-1px);
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+}
+
+.btn-white {
+    background-color: #fff;
+    color: #777;
+}
+
+.btn::after {
+    content: "";
+    display: inline-block;
+    height: 100%;
+    width: 100%;
+    border-radius: 100px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    transition: all .4s;
+}
+
+.btn-white::after {
+    background-color: #fff;
+}
+
+.btn:hover::after {
+    transform: scaleX(1.4) scaleY(1.6);
+    opacity: 0;
+}
+
+.btn-animated {
+    animation: moveInBottom 5s ease-out;
+    animation-fill-mode: backwards;
+}
+
+@keyframes moveInBottom {
+    0% {
+        opacity: 0;
+        transform: translateY(30px);
+    }
+
+    100% {
+        opacity: 1;
+        transform: translateY(0px);
+    }
+}
+
+</style>
