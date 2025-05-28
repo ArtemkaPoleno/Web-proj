8dba2d2b6944a566a49ecf7016d9cb4ae92696bf
ArtemkaPoleno <hdh16788@gmail.com>
2025-05-28 18:02:21 +0400

    Update index.vue

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,52 +1,69 @@
 <script setup>
-const { data: posts } = await useAsyncData('posts', () => 
-  $fetch('http://localhost:3001/posts')
-);
+
+const { data: posts, refresh } = await useAsyncData(
+  'all-posts',
+  () => $fetch('http://localhost:3001/posts'),
+  {
+    getCachedData: () => undefined,
+    transform: (data) => data 
+  }
+)
+
+
+onMounted(() => refresh())
 </script>
+
 <template>
   <AppHeader />
   <NuxtPage />
+  
   <div class="posts-page">
-    <h1>Все посты </h1>
-    <div class="content">
-      <div class="post" v-for="post in posts" :key="post.id">
-        <NuxtLink :to="`/posts/${post.id}`" class="post-link">
-          <h2>{{ post.title }}</h2>
-          <div class="post-content">
-            <p>{{ post.content }}</p>
-          </div>
-          <span class="read-more">Читать дальше →</span>
-        </NuxtLink>
+    <h1>Все посты</h1>
+    
+    <div v-if="pending">Загрузка...</div>
+    
+    <div v-else class="content">
+      <div class="post-grid">
+        <div class="post" v-for="post in posts" :key="post.id">
+          <NuxtLink :to="`/posts/${post.id}`" class="post-link">
+            <h2>{{ post.title }}</h2>
+            <div class="post-content">
+              <p>{{ post.content }}</p>
+            </div>
+            <span class="read-more">Читать дальше →</span>
+          </NuxtLink>
+        </div>
+      </div>
+      
+      <div class="btn-div">
+        <NuxtLink to="/" class="btn btn-white btn-animate">На главную</NuxtLink>
       </div>
     </div>
-    <NuxtLink to="/" class="btn btn-white btn-animate">На главную</NuxtLink>
   </div>
 </template>
 
-
-
 <style lang="scss">
 .posts-page {
-  max-width: 1536px;
+  
   margin: 0 auto;
 }
-
-.content {
-  display: grid;
-  grid-template-columns:1fr 1fr 1fr;
-  gap: 20px;
-
+.btn-div{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  margin-top: 30px;
 }
-
 .post {
   border: 1px solid #eee;
   border-radius: 8px;
   padding: 20px;
   transition: transform 0.3s ease;
-  height: 250px; /* Фиксированная высота карточки */
+  height: 250px;
   display: flex;
   flex-direction: column;
-  overflow: hidden; /* Скрываем текст, выходящий за границы */
+
 
   &:hover {
     transform: translateY(-5px);
@@ -68,11 +85,11 @@ h2 {
   margin-bottom: 15px;
   font-size: 1.2rem;
   line-height: 1.4;
-  word-wrap: break-word; /* Перенос длинных слов */
-  overflow-wrap: break-word; /* Альтернатива для некоторых браузеров */
-  word-break: break-word; /* Переносит слишком длинные слова */
+  word-wrap: break-word; 
+  overflow-wrap: break-word;
+  word-break: break-word;
   
-  /* Ограничение по количеству строк */
+
   display: -webkit-box;
   -webkit-line-clamp: 2;
   -webkit-box-orient: vertical;
@@ -93,7 +110,7 @@ h2 {
     word-break: break-word;
     text-overflow: ellipsis;
     
-    /* Ограничение по количеству строк */
+
     display: -webkit-box;
     -webkit-line-clamp: 4;
     -webkit-box-orient: vertical;
@@ -123,7 +140,6 @@ h2 {
   }
 }
 
-/* Добавляем градиент для плавного обрезания текста */
 .post-content {
   position: relative;
   
