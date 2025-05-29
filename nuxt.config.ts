d74d81365069099b6eef6b56044d1acbb78a86a3
ArtemkaPoleno <hdh16788@gmail.com>
2025-05-29 12:17:11 +0400

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  
+   modules: ['@nuxt/fonts'],
+   fonts: {
+    families: [
+      { name: 'Roboto', provider: 'google' }
+    ]
+  },
+  app: {
+    head: {
+      title: 'IT блог'
+      //titleTemplate: '%s | IT блог', //
+    }}
 });
