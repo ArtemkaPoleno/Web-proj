0b6a39b7bfe102805058d2e692fae569a1611acf
ArtemkaPoleno <hdh16788@gmail.com>
2025-05-30 17:28:27 +0400

    Update AppHeader.vue

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,40 +1,39 @@
-<script setup>
-
-</script>
+<script setup></script>
 <template>
   <header class="header">
     <div class="header__container">
-      <div class="text-box"><NuxtLink to="/posts" class="btn btn-white btn-animate">Посты</NuxtLink></div>
+      <div class="text-box">
+        <NuxtLink to="/posts" class="btn btn-white btn-animate">Посты</NuxtLink>
+      </div>
       <NuxtLink to="/" class="header__logo">
-        <img src="~/assets/images/logo.svg" alt="Логотип" class="logo-image">
+        <img src="~/assets/images/logo.svg" alt="Логотип" class="logo-image" >
       </NuxtLink>
-      <div class="text-box"><NuxtLink to="/create" class="btn btn-white btn-animate">Создать пост</NuxtLink></div>
-     
+      <div class="text-box">
+        <NuxtLink to="/create" class="btn btn-white btn-animate"
+          >Создать пост</NuxtLink
+        >
+      </div>
     </div>
   </header>
 </template>
 
-
 <style>
 .header__container {
-    margin-bottom:20px;
-    display:flex;
-    
-    justify-content:space-between;
-    align-items:center;
-    background: #ffffff;
-    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-    .logo-image {
-      height: 100px;
-      width: auto;
-      transition: transform 0.3s ease;
+  margin-bottom: 20px;
+  display: flex;
 
-      &:hover {
-        transform: scale(1.05);
-      }
+  justify-content: space-between;
+  align-items: center;
+  background: #ffffff;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  .logo-image {
+    height: 100px;
+    width: auto;
+    transition: transform 0.3s ease;
+
+    &:hover {
+      transform: scale(1.05);
     }
   }
-
-
-
-  </style>
+}
+</style>
