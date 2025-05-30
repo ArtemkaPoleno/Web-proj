7061b37f0c5d600560708917ed616f60ec2ce281
ArtemkaPoleno <hdh16788@gmail.com>
2025-05-30 17:31:08 +0400

    Update index.vue

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,53 +1,48 @@
 <script setup>
+const api = useFetchApi();
+const { data: posts, status } = await useLazyAsyncData(
+  "all-posts",
+  () => api("/posts"),
 
-const { data: posts, refresh } = await useAsyncData(
-  'all-posts',
-  () => $fetch('http://localhost:3001/posts'),
-  {
-    getCachedData: () => undefined,
-    transform: (data) => data 
-  }
-)
+);
 
 
-onMounted(() => refresh())
 </script>
 
 <template>
-  <AppHeader />
-  <NuxtPage />
-  
-  <div class="posts-page">
-    <h1>Все посты</h1>
-    
-    <div v-if="pending">Загрузка...</div>
-    
-    <div v-else class="content">
-      <div class="post-grid">
-        <div class="post" v-for="post in posts" :key="post.id">
-          <NuxtLink :to="`/posts/${post.id}`" class="post-link">
-            <h2>{{ post.title }}</h2>
-            <div class="post-content">
-              <p>{{ post.content }}</p>
-            </div>
-            <span class="read-more">Читать дальше →</span>
-          </NuxtLink>
+    <div class="posts-page">
+      <h1>Все посты</h1>
+      <div v-if="status == 'success'">
+        <div class="content">
+          <div class="post-grid">
+            <div v-for="post in posts" :key="post.id" class="post" >
+              <NuxtLink :to="`/posts/${post.id}`" class="post-link">
+              <h2>{{ post.title }}</h2>
+              <div class="post-content">
+                <p>{{ post.content }}</p>
+              </div>
+              <span class="read-more">Читать дальше →</span>
+            </NuxtLink>
+          </div>
+        </div>
+
+        <div class="btn-div">
+          <NuxtLink to="/" class="btn btn-white btn-animate"
+            >На главную</NuxtLink
+          >
         </div>
       </div>
-      
-      <div class="btn-div">
-        <NuxtLink to="/" class="btn btn-white btn-animate">На главную</NuxtLink>
-      </div>
     </div>
+      <div v-else>Загрузка...</div>
   </div>
+
 </template>
 
 <style lang="scss">
 .posts-page {
-  
   margin: 0 auto;
 }
-.btn-div{
+.btn-div {
   display: flex;
   align-items: center;
   justify-content: center;
@@ -64,10 +59,9 @@ onMounted(() => refresh())
   display: flex;
   flex-direction: column;
 
-
   &:hover {
     transform: translateY(-5px);
-    box-shadow: 0 5px 15px rgba(0,0,0,0.1);
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1);
   }
 }
 
@@ -85,12 +79,12 @@ h2 {
   margin-bottom: 15px;
   font-size: 1.2rem;
   line-height: 1.4;
-  word-wrap: break-word; 
+  word-wrap: break-word;
   overflow-wrap: break-word;
   word-break: break-word;
-  
 
   display: -webkit-box;
+  line-clamp: 2;
   -webkit-line-clamp: 2;
   -webkit-box-orient: vertical;
   overflow: hidden;
@@ -100,7 +94,7 @@ h2 {
   flex-grow: 1;
   margin-bottom: 30px;
   overflow: hidden;
-  
+
   p {
     color: #7f8c8d;
     margin: 0;
@@ -109,14 +103,13 @@ h2 {
     overflow-wrap: break-word;
     word-break: break-word;
     text-overflow: ellipsis;
-    
 
     display: -webkit-box;
+    line-clamp: 4;
     -webkit-line-clamp: 4;
     -webkit-box-orient: vertical;
     overflow: hidden;
   }
-  
 }
 
 .read-more {
@@ -143,15 +136,14 @@ h2 {
 
 .post-content {
   position: relative;
-  
+
   &::after {
-    content: '';
+    content: "";
     position: absolute;
     bottom: 0;
     left: 0;
     right: 0;
     height: 30px;
-    
   }
 }
 </style>
