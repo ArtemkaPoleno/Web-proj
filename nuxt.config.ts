ea7c22e793a8984a746ad52fbc4817306b65e05f
ArtemkaPoleno <hdh16788@gmail.com>
2025-05-30 17:30:58 +0400

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-   modules: ['@nuxt/fonts'],
-   fonts: {
-    families: [
-      { name: 'Roboto', provider: 'google' }
-    ]
+  modules: ["@nuxt/fonts", "@nuxt/eslint"],
+  fonts: {
+    families: [{ name: "Roboto", provider: "google" }],
   },
   app: {
     head: {
-      title: 'IT блог'
+      title: "IT блог",
       //titleTemplate: '%s | IT блог', //
-    }}
+    },
+  },
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.API_BASE_URL || 'http://localhost:3001' // Для json-server
+    }
+  },
 });
