13aba2a3583a7a8451db8b6b08c4c25665c052b8
ArtemkaPoleno <hdh16788@gmail.com>
2025-06-02 01:30:53 +0400

    Create create.vue

---
+++ pages/posts/create.vue
@@ -0,0 +1,24 @@
+<script setup>
+const title = ref("");
+const content = ref("");
+
+const createPost = async () => {
+  await $fetch("http://localhost:3001/posts", {
+    method: "POST",
+    body: {
+      title: title.value,
+      content: content.value,
+    },
+  });
+  navigateTo("/posts");
+};
+</script>
+<template>
+  <div>
+    <h1>Создать пост</h1>
+    <input v-model="title" placeholder="Заголовок" />
+    <textarea v-model="content" placeholder="Текст"></textarea>
+    <button @click="createPost">Опубликовать</button>
+    <NuxtLink to="/posts">Отмена</NuxtLink>
+  </div>
+</template>
